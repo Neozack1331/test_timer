0499209ae2a6f663f62dc2b4fc47e15cd2c11832
Your Name <your.email@example.com>
2025-06-21 20:49:50 +0900

    Update eventlet version and async mode

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ eventlet.monkey_patch()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', os.urandom(24))
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
 timer_running = False
 current_time = 0
